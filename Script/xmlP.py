c7b42ece03e0562906d0ae4339cc82fffe99c904
sagar <sagar@sagardeMacBook-Air.local>
2017-02-11 22:32:56 +0800

    Update

--- Script/xmlP.py
+++ Script/xmlP.py
@@ -15,14 +15,13 @@ try:
 except:
     print "Unable to connect"
 
+def testcaseNumberHitInsertFromXml():
+    doc = minidom.parse("tc7_F.xml")
 
-
-doc = minidom.parse("tc7_F.xml")
-
-# doc.getElementsByTagName returns NodeList
-testcaseID = 7
-linesNumber = doc.getElementsByTagName("line")
-for linesNumberofStatement in linesNumber:
+    # doc.getElementsByTagName returns NodeList
+    testcaseID = 7
+    linesNumber = doc.getElementsByTagName("line")
+    for linesNumberofStatement in linesNumber:
         hitsofEachStatement = linesNumberofStatement.getAttribute("hits")
         statementNumber = linesNumberofStatement.getAttribute("number")
         print("Statement Number:%s, Hit:%s" %
@@ -33,3 +32,21 @@ for linesNumberofStatement in linesNumber:
             connection.commit();
         except:
             print "problem found"
+
+def all_failed_testcases():
+    """Listing all the failed testcases(a11+a01) from the database """
+    cursor.execute("SELECT COUNT(*) FROM Testcase WHERE testcaseStatus = 0")
+    data = cursor.fetchone()[0]
+    print data
+
+
+
+def all_passed_testcases():
+    """Listing all the passed testcases(a10+a00) from the database """
+    cursor.execute("SELECT COUNT(*) FROM Testcase WHERE testcaseStatus >= 1")
+    data = cursor.fetchone()[0]
+    return data
+
+#all_failed_testcases()
+a10_a00 = all_passed_testcases()
+#print a10_a00
